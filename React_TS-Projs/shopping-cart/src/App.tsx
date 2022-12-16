b47a458e15c4ed52f7431fd531aa5e77d6f58dd9
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-12-16 15:24:09 +0100

    Routing

--- React_TS-Projs/shopping-cart/src/App.tsx
+++ React_TS-Projs/shopping-cart/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+import { About, Home, Store } from './pages';
 
 function App() {
 
   return (
-    <div className="App">
-      Hi
-    </div>
+   <Container className="mb-4">
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+     </Routes>
+   </Container>
   )
 }
 
---
+++ React_TS-Projs/shopping-cart/src/pages/Store.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type StoreProps = {
+
+}
+
+const Store: React.FC<StoreProps> = ({}) => {
+    return (
+        <div>
+            Store
+        </div>
+    )
+}
+
+export default Store
