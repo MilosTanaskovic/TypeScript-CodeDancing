ce0ecf1ee9e50a68a0b90ef90ef3b7eafc275987
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-12-16 15:55:49 +0100

    NavBar Comp

--- React_TS-Projs/shopping-cart/src/App.tsx
+++ React_TS-Projs/shopping-cart/src/App.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom';
 import {Container} from 'react-bootstrap';
 import { About, Home, Store } from './pages';
+import { NavBar } from './components';
 
 function App() {
 
   return (
-   <Container className="mb-4">
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/about" element={<About />} />
-     </Routes>
-   </Container>
+    <>
+      <NavBar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+   </>
   )
 }
 
